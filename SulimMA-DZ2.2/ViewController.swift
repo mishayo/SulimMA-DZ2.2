18e5c45b757287de35f02eb0e9c46d6a7f1fae20
Mikhail <s_m86@mail.ru>
2021-03-27 03:15:43 +0300

    dz2.2

--- SulimMA-DZ2.2/ViewController.swift
+++ SulimMA-DZ2.2/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorBar: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorBar.layer.cornerRadius = 15
+        redLabel.text = "\(redSlider.value)"
+        greenLabel.text = "\(greenSlider.value)"
+        blueLabel.text = "\(blueSlider.value)"
+        
+        colorBar.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
     }
 
-
+    @IBAction func redSliderMove(_ sender: UISlider) {
+        redLabel.text = String(format: "%.2f", sender.value)
+        colorBar.backgroundColor = UIColor.init(displayP3Red: CGFloat(sender.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func greenSliderMove(_ sender: UISlider) {
+        greenLabel.text = String(format: "%.2f", sender.value)
+        colorBar.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(sender.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func blueSliderMove(_ sender: UISlider) {
+        blueLabel.text = String(format: "%.2f", sender.value)
+        colorBar.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(sender.value), alpha: 1)
+    }
+    
+    
 }
 
